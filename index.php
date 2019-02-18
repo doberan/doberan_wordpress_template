5eac1574a5ae5ac2dd3198c8a48bb6d192db9ac8
doberankei <masayathegantz@gmail.com>
2019-02-19 02:41:54 +0900

    create HARIBOTE

---
+++ index.php
@@ -0,0 +1,125 @@
+<?php
+
+?>
+<!-- 
+  wordpress template
+
+ -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf8" />
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <header>
+      doberan blog template
+    </header>
+    <main>
+      <div id="top" class="containts">
+        <h2 class="contains-title">記事一覧</h2>
+        <article class="list">
+          <div class="image-box"><img src="#" alt="" /></div>
+          <div class="detail-box">
+            <h3 class="article-title">title</h3>
+            <p class="article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+          </div>
+        </article>
+
+        <article class="list">
+          <div class="image-box"><img src="#" alt="" /></div>
+          <div class="detail-box">
+            <h3 class="article-title">title</h3>
+            <p class="article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+          </div>
+        </article>
+
+        <article class="list">
+          <div class="image-box"><img src="#" alt="" /></div>
+          <div class="detail-box">
+            <h3 class="article-title">title</h3>
+            <p class="article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+          </div>
+        </article>
+
+        <article class="list">
+          <div class="image-box"><img src="#" alt="" /></div>
+          <div class="detail-box">
+            <h3 class="article-title">title</h3>
+            <p class="article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+          </div>
+        </article>
+
+        <article class="list">
+          <div class="image-box"><img src="#" alt="" /></div>
+          <div class="detail-box">
+            <h3 class="article-title">title</h3>
+            <p class="article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+          </div>
+        </article>
+
+        <article class="list">
+          <div class="image-box"><img src="#" alt="" /></div>
+          <div class="detail-box">
+            <h3 class="article-title">title</h3>
+            <p class="article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+          </div>
+        </article>
+
+        <article class="list">
+          <div class="image-box"><img src="#" alt="" /></div>
+          <div class="detail-box">
+            <h3 class="article-title">title</h3>
+            <p class="article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+          </div>
+        </article>
+      </div>
+      <div class="sidebar">
+        <div class="tag-cloud">
+          <h2 class="tag-cloud-title">カテゴリ</h2>
+          <div class="tag-cloud-box">
+            <div class="tag-box"><p class="tag">テスト</p></div>
+            <div class="tag-box"><p class="tag">HTML</p></div>
+            <div class="tag-box"><p class="tag">Rust</p></div>
+            <div class="tag-box"><p class="tag">JavaScript</p></div>
+            <div class="tag-box"><p class="tag">Vue.js</p></div>
+          </div>
+        </div>
+        <div class="ranking">
+          <h2 class="ranking-title">ランキング</h2>
+          <article class="ranking-article-list">
+            <div class="ranking-image-box"><img src="#" alt="" /></div>
+            <div class="ranking-detail-box">
+              <h3 class="ranking-article-title">title</h3>
+              <p class="ranking-article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+            </div>
+          </article>
+          <article class="ranking-article-list">
+            <div class="ranking-image-box"><img src="#" alt="" /></div>
+            <div class="ranking-detail-box">
+              <h3 class="ranking-article-title">title</h3>
+              <p class="ranking-article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+            </div>
+          </article>
+          <article class="ranking-article-list">
+            <div class="ranking-image-box"><img src="#" alt="" /></div>
+            <div class="ranking-detail-box">
+              <h3 class="ranking-article-title">title</h3>
+              <p class="ranking-article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+            </div>
+          </article>
+          <article class="ranking-article-list">
+            <div class="ranking-image-box"><img src="#" alt="" /></div>
+            <div class="ranking-detail-box">
+              <h3 class="ranking-article-title">title</h3>
+              <p class="ranking-article-description">本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。本日は晴天なり。</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <small>2019, &copy doberan. </small>
+    </footer>
+  </body>
+</html>
